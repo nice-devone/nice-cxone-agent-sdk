41d93191af6d8c536d373cfa4d350d2bd289989d
Latika Singh <latika.singh@nice.com>
2025-12-08 15:18:36 +0530

    updated cxa placeholder with contact switch

--- cxa-sdk-consumer/src/components/cxa-placeholder/CxaPlaceholder.tsx
+++ cxa-sdk-consumer/src/components/cxa-placeholder/CxaPlaceholder.tsx
@@ -11,8 +11,10 @@
  * 
  * @returns {JSX.Element} A div element with id "launchCXA" that serves as a placeholder for the CXone Agent.
  */
+import { Button } from '@mui/material';
+import { CXoneClient } from '@nice-devone/agent-sdk';
 import { CXoneAuth } from '@nice-devone/auth-sdk';
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 
 const CxaPlaceholder=()=>{
@@ -26,8 +28,21 @@ const CxaPlaceholder=()=>{
                       { width: "400px", height: "500px" }
                     );
     },[])
-    return(
-        <div id="launchCXA">CXA Placeholder </div>
-    )
+
+  const handleEvent = () => {
+    const sessionDetails =
+    {
+      interactionId: '060a4e8a-238d-4866-8755-1f94a9f45d65',
+      contactId: '316436517', mediaType: 'Digital'
+    }
+
+    CXoneClient.instance.switchContacts(sessionDetails);
+  }
+  return (
+    <>
+      <Button onClick={handleEvent}>fire event</Button>
+      <div id="launchCXA">CXA Placeholder </div>
+    </>
+  )
 }
 export default CxaPlaceholder;
